3cf3f5c52c277d1934a96b8491d0a9eb836ea1bb
Cccc-owo <net4hhhh@163.com>
2022-11-28 18:10:31 +0800

    build: update main.bat

--- main.bat
+++ main.bat
@@ -9,23 +9,23 @@ ECHO.
 ECHO start copying
 ::integrating files
 ECHO.
-cp -r eu4_chinese/* output/ETCP/
-cp -r eu4_chinese_sup/* output/ETCP/
-cp -r ExtendedTimeline/* output/ETCP/
-cp -r cn_text/* output/ETCP/
-cp -r addition/* output/ETCP/
-cp -r mod_descriptor/* output/
+xcopy eu4_chinese\ output\ETCP\ /E /Q /C /Y
+xcopy eu4_chinese_sup\ output\ETCP\ /E /Q /C /Y
+xcopy ExtendedTimeline\ output\ETCP\ /E /Q /C /Y
+xcopy cn_text\ output\ETCP\ /E /Q /C /Y
+xcopy addition\ output\ETCP\ /E /Q /C /Y
+xcopy mod_descriptor\ output\ /E /Q /C /Y
 ::post-processing
 ECHO.
 ECHO Copying the file is finished, finishing the aftermath...
 ECHO.
 del output\ETCP\thumbnail.png
-del /F /S /Q output\ETCP\localisation\*_l_spanish.yml
-del /F /S /Q output\ETCP\localisation\*_l_french.yml
-del /F /S /Q output\ETCP\localisation\*_l_german.yml
+del /F /S /Q output\ETCP\localisation\_l_spanish.yml
+del /F /S /Q output\ETCP\localisation\_l_french.yml
+del /F /S /Q output\ETCP\localisation\_l_german.yml
 ECHO.
 ECHO Compressing the generated files
-7z.exe a ETCP.zip .\output\*
+7z.exe a ETCP.zip .\output\
 ::done
 ECHO.
 ECHO End of script running
