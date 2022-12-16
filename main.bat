c7dad4710d1bf50ddfa7df461f9035aad48ff6b1
Cccc-owo <net4hhhh@163.com>
2022-12-16 09:42:47 +0800

    chore: update script[skip ci]

--- main.bat
+++ main.bat
@@ -4,7 +4,7 @@ set mod_dir=ETCLP
 ::checking
 ECHO Time:%date%
 ECHO Checking the required folder...
-if not exist output\ ( mkdir \output\ ) else ( del /F /S /Q output\ & rmdir /S /Q output\ & mkdir output\ )
+if not exist output\ ( mkdir output\ ) else ( del /F /S /Q output\ & rmdir /S /Q output\ & mkdir output\ )
 if not exist output\%mod_dir% mkdir output\%mod_dir%\
 if exist mod.zip del /F /S /Q mod.zip
 ::pre-processing
