baebf84e9e16f18fcb12d490496ca5878551efa2
Cccc-owo <net4hhhh@163.com>
2023-02-11 21:06:27 +0800

    website: init

---
+++ website/docs/.vuepress/config.ts
@@ -0,0 +1,23 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/en/": {
+      lang: "en-US",
+      title: "Docs Demo",
+      description: "A docs demo for vuepress-theme-hope",
+    },
+    "/": {
+      lang: "zh-CN",
+      title: "文档演示",
+      description: "vuepress-theme-hope 的文档演示",
+    },
+  },
+
+  theme,
+
+  shouldPrefetch: false,
+});
